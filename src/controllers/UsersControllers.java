d4b199971330a2c7397b6960ac6e30cd16d32307
Nicolas10dz <nicolas10dz@gmail.com>
2026-03-06 02:39:11 -0500

    Se creo Users Controller, con getters geter para id y delete con id

---
+++ src/controllers/UsersControllers.java
@@ -0,0 +1,95 @@
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import com.Aldea.Naruto.dto.MessageResponseDTO;
+import com.Aldea.Naruto.dto.UsersRequestDTO;
+import com.Aldea.Naruto.dto.UsersResponseDTO;
+import com.Aldea.Naruto.services.UsersService;
+
+public class UsersControllers {
+
+    @RestController
+    @RequestMapping("/users")
+    public class UsersController {
+
+
+        private final UsersService usersService;
+        //Inyeccion de dependencias
+
+
+        public UsersController(UsersService usersService) {
+            this.usersService = usersService;
+        }
+
+
+        @PostMapping()
+        public ResponseEntity <UsersRequestDto> createUser(@RequestBody UsersRequestDTO usersRequestDto){
+
+            UserResponseDTO response = usersService.createUser(usersRequestDto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+
+        }
+
+        @GetMapping()
+    public ResponseEntity <List<UsersResponseDTO>> getUsers(){
+    List<UsersResponseDTO> response = usersService.getUser();
+    return ResponseEntity.status(HttpStatus.FOUND).body(response);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity <UsersResponseDTO> getUser(@PathVariable Long id){
+        UsersResponseDTO response = usersService.getUser(id);
+        return ResponseEntity.status(HttpStatus.FOUND).body(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity <MessageResponseDTO> deleteUser(@PathVariable Long id){
+        MessageResponseDTO response = usersService.deleteUser(id);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    
+        
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
